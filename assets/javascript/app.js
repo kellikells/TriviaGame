dece01758b04467a9db2c6d6810832ba7290f354
K Uyeda <kellikells@gmail.com>
2020-01-28 17:57:46 -0800

    changing entire js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,69 @@
-
-
-
-// figuring out how to use objects instead of arrays for accessing data
-
-//==================  GLOBAL Variables ==========================
-
-
+var qArr = [{
+    "question": "What color is associated with Team Valor?",
+    "choices": ["red", "blue", "green", "blue"],
+    "answer": "red"
+},
+{
+    "question": "some question?",
+    "choices": ["yellow", "work", "green", "blue"],
+    "answer": "work"
+}];
 
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
-
 //---- max number is 6, use this for LOGIC when <button>s are clicked
 var currentNumOfQs = correct + incorrect + unanswered;
 
-//----- tracks the number of <button> clicks (any)
-var clickCounter = 0;
+var currentQuestion = 0;
+
+function displayQuestions() {
+    $("#question").append(qArr[currentQuestion]);
+    $("#bttn1").attr("value", qArr[currentQuestion].choices[0]);
+    $("#bttn2").attr("value", qArr[currentQuestion].choices[1]);
+    $("#bttn3").attr("value", qArr[currentQuestion].choices[2]);
+    $("#bttn4").attr("value", qArr[currentQuestion].choices[3]);
+    $("#bttn1").attr("rightAnswer", qArr[currentQuestion].answer);
+    $("#bttn2").attr("rightAnswer", qArr[currentQuestion].answer);
+    $("#bttn3").attr("rightAnswer", qArr[currentQuestion].answer);
+    $("#bttn4").attr("rightAnswer", qArr[currentQuestion].answer);
+    $("#bttn1").html(qArr[currentQuestion].choices[0]);
+    $("#bttn2").html(qArr[currentQuestion].choices[1]);
+    $("#bttn3").html(qArr[currentQuestion].choices[2]);
+    $("#bttn4").html(qArr[currentQuestion].choices[3]);
+    if (currentQuestion < qArr.length - 1) {
+        currentQuestion++;
+    }
+    else {
+        // calling a function that displays the results
+    }
+}
+
+// user clicks an answer > correct or incorrect
+$(".bttn").on("click", function (event) {
+    event.preventDefault();
+    var userChoice=$(this).attr("value");
+    var rightAnswer=$(this).attr("rightAnswer");
+if (userChoice === rightAnswer) {
+    correct++;
+}
+else {
+    incorrect++;
+}
+displayQuestions();
+})
+
+$(".bttn").hide();
+
+// var correct = 0;
+// var incorrect = 0;
+// var unanswered = 0;
+// //---- max number is 6, use this for LOGIC when <button>s are clicked
+// var currentNumOfQs = correct + incorrect + unanswered;
+
+// var currentQuestion = 0;
+//----- tracks the number of =  <button> clicks (any)
+// var clickCounter = 0;
 // ==================FUNCTIONS=========================
 
 // ----------------------------------------------
@@ -42,12 +90,15 @@ function decrement() {
 // ----------------------------------------------
 // start/play button functions
 // ----------------------------------------------
-$("#start-bttn").on("click", startGame);
+$("#start").on("click", function (event) {
+    // startTimer();
+    event.preventDefault();
+    console.log("message something");
+    $("#start").hide();
+    $(".bttn").show();
+    displayQuestions();
 
-var startGame = function () {
-    startTimer();
-    
-    }
+})
 
 // ----------------------------------------------
 // answer buttons functions
@@ -60,122 +111,107 @@ var startGame = function () {
 //     display the question & answers 
 
 
-var displayQuestions = function pickPageToDisplay() {
+// var displayQuestions = function pickPageToDisplay() {
 
-}
-// ----------------------------------------------
-// replaceWith()
-// $("start")
-
-
-// $(document).ready(function() {
-
-//     $("<button>start</button>").insertAfter("#start");
-// })
-
-
-$("<button>start</button>").insertAfter("#start");
-}
-
-
-
-
-
-// $("#bttn").on("click", evaluate);
-
-
-// //  --- after 30 seconds it moves to next q & increases unanswered count
-// var thirtySec = setTimeout(unansweredThirtySec, 1000 * 30);
-
-// function unansweredThirtySec() {
-//     let 
-//     if (correct++ || incorrect++) {
-//         clearTimeout(unansweredThirtySec);
-//     }
 // }
+// // ----------------------------------------------
+// // replaceWith()
+// // $("start")
 
 
-// function unansweredThirtySec() {
+// // $(document).ready(function() {
 
-//     // start q      or      summary q
-//     if ((currentNumOfQs === 0) || (currentNumOfQs === 6)) {
-//         clearTimeout(thirtySec);
+// //     $("<button>start</button>").insertAfter("#start");
+// // })
 
-//     }
+
+// $("<button>start</button>").insertAfter("#start");
 // }
 
 
 
 
 
+// // $("#bttn").on("click", evaluate);
+
+
+// // //  --- after 30 seconds it moves to next q & increases unanswered count
+// // var thirtySec = setTimeout(unansweredThirtySec, 1000 * 30);
+
+// // function unansweredThirtySec() {
+// //     let 
+// //     if (correct++ || incorrect++) {
+// //         clearTimeout(unansweredThirtySec);
+// //     }
+// // }
+
+
+// // function unansweredThirtySec() {
+
+// //     // start q      or      summary q
+// //     if ((currentNumOfQs === 0) || (currentNumOfQs === 6)) {
+// //         clearTimeout(thirtySec);
+
+// //     }
+// // }
 
 
 
 
 
 
+// // =========================================================
+// // +++++++++++DOESN'T WORK / NOT FINISHED++++++++++++++++++++
 
 
+// // -------specific to the page-----
+// //  ----will hold the correct answer, followed by wrong options 
+// var pageSpecificArray = [];
 
-
-
-
-
-
-
-
-// =========================================================
-// +++++++++++DOESN'T WORK / NOT FINISHED++++++++++++++++++++
-
-
-// -------specific to the page-----
-//  ----will hold the correct answer, followed by wrong options 
-var pageSpecificArray = [];
+// // function objectToArray() {
+// //     $this.pageSpecificArray.push(this.answer);
+// //     this.pageSpecificArray.push(this.wrongOne);
+// //     this.pageSpecificArray.push(this.wrongTwo);
+// //     this.pageSpecificArray.push(this.wrongThree);
+// // }
 
 // function objectToArray() {
-//     this.pageSpecificArray.push(this.answer);
-//     this.pageSpecificArray.push(this.wrongOne);
-//     this.pageSpecificArray.push(this.wrongTwo);
-//     this.pageSpecificArray.push(this.wrongThree);
+//     pageSpecificArray.push(this.answer);
+//     pageSpecificArray.push(this.wrongOne);
+//     pageSpecificArray.push(this.wrongTwo);
+//     pageSpecificArray.push(this.wrongThree);}
+
+// var qOne;
+// var qTwo;
+// var qThree;
+// var qFour;
+// var qFive;
+// var qSix;    
+
+// // declaring the Object Constructor function, createQuestionAnswerObj 
+// // Could have made each Object, hopefully this approach will be useful in future applications
+// function createQuestionAnswerObj(question, answer, wrongOne, wrongTwo, wrongThree) {
+//     this.question = question;
+//     this.answer = answer;
+//     this.wrongOne = wrongOne;
+//     this.wrongTwo = wrongTwo;
+//     this.wrongThree = wrongThree;
 // }
 
-function objectToArray() {
-    pageSpecificArray.push(this.answer);
-    pageSpecificArray.push(this.wrongOne);
-    pageSpecificArray.push(this.wrongTwo);
-    pageSpecificArray.push(this.wrongThree);}
-
-var qOne;
-var qTwo;
-var qThree;
-var qFour;
-var qFive;
-var qSix;
-
-// declaring the Object Constructor function, createQuestionAnswerObj 
-// Could have made each Object, hopefully this approach will be useful in future applications
-function createQuestionAnswerObj(question, answer, wrongOne, wrongTwo, wrongThree) {
-    this.question = question;
-    this.answer = answer;
-    this.wrongOne = wrongOne;
-    this.wrongTwo = wrongTwo;
-    this.wrongThree = wrongThree;
-}
-
-// calling the Obj Constructor function
-qOne = new createQuestionAnswerObj("What color is associated with Team Valor?","Red", "Blue", "Yellow", "Green");
-qTwo = new createQuestionAnswerObj("Pokemon Go generated $__ million in revenue its first month","200", "50", "150", "100");
-qThree = new createQuestionAnswerObj("What name guarantees Eevee becoming Flareon?", "Pyro", "Rainer", "Spicy", "Sparky");
-qFour = new createQuestionAnswerObj("Who is the Yellow team's mascot?", "Zapdos", "Spark", "Pikachu", "Instinct");
-qFive = new createQuestionAnswerObj("What berry makes it easier to catch a pokemon", "razz berry", "pinap berry", "lucky berry", "blueberry");
-qSix = new createQuestionAnswerObj("What is the highest level a trainer can reach?", "40", "35", "75", "100");
+// // calling the Obj Constructor function
+// qOne = new createQuestionAnswerObj("What color is associated with Team Valor?","Red", "Blue", "Yellow", "Green");
+// qTwo = new createQuestionAnswerObj("Pokemon Go generated $__ million in revenue its first month","200", "50", "150", "100");
+// qThree = new createQuestionAnswerObj("What name guarantees Eevee becoming Flareon?", "Pyro", "Rainer", "Spicy", "Sparky");
+// qFour = new createQuestionAnswerObj("Who is the Yellow team's mascot?", "Zapdos", "Spark", "Pikachu", "Instinct");
+// qFive = new createQuestionAnswerObj("What berry makes it easier to catch a pokemon", "razz berry", "pinap berry", "lucky berry", "blueberry");
+// qSix = new createQuestionAnswerObj("What is the highest level a trainer can reach?", "40", "35", "75", "100");
 
 
-console.log(qOne.question);
+// console.log(qOne.question);
 
 
 
 
-// ++++++++++++++++++++++++++++++++++++++++++++++++
-// window.onload = function () {
+// // ++++++++++++++++++++++++++++++++++++++++++++++++
+// // window.onload = function () {
 
--- assets/javascript/first-approach.js
+++ assets/javascript/first-approach.js
@@ -1,80 +1,149 @@
-// ======================================================
-//==================  VARIABLES needed ==========================
-// ======================================================
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+//---- max number is 6, use this for LOGIC when <button>s are clicked
+var currentNumOfQs = correct + incorrect + unanswered;
 
-// conditional statements will compare the values 
+var currentQuestion = 0;
+----- tracks the number of =  <button> clicks (any)
+var clickCounter = 0;
+==================FUNCTIONS=========================
 
-let questionArrayVal1 = [
-    "What color is associated with Team Valor?",
-    "Pokemon Go generated __ more revenue than Candy Crush in its first month",
-    "What do you rename Evee to nearly guarantee a Flareon while evolving?",
-    "How many people play Pokemon Go everyday? (approx)",
-    "Who is the yellow team's mascot?",
-    "What year was Pokemon Go released?",
-    "Which item makes it easier to catch a Pokemon",
-    "What is the highest level a trainer can obtain?"
-];
+----------------------------------------------
+---- timer for 30 seconds --- setInterval
+----------------------------------------------
+var seconds = 30;
+var thirtySecTimer;
 
-let correctVal2A1 = [
-    "red",
-    "7",
-    "pyro",
-    "5 million",
-    "Zapdos",
-    "2016",
-    "razz berry",
-    "40"];
+function startTimer() {
+    clearInterval(thirtySecTimer);
+    thirtySecTimer = setInterval(decrement, 1000 * 30);
+    $("#intervalTimer").append("Time Left: " + seconds);
+}
 
+function decrement() {
+    seconds--;
+    // $("#intervalTimer").html("Time remaining: " + seconds + "!");
+    if (seconds <= 0) {
+        unanswered++;
+    }
+}
 
+// start/play button functions
+// ----------------------------------------------
+$("#start").on("click", function (event) {
+    startTimer();
+    event.preventDefault();
+    displayQuestions();
+})
 
-let wrongVal3A2 = [
-    "blue",
-    "2",
-    "Rainer",
-    "1 million",
-    "Spark",
-    "2015",
-    "pinap berry",
-    "35"
-];
+// ----------------------------------------------
+// answer buttons functions
+//  check if user click = correct   
 
-let wrongVal4A3 = [
-    "yellow",
-    "5",
-    "Spicy",
-    "2 million",
-    "Pikachu",
-    "2017",
+// ----------------------------------------------
 
-]
-
-let wrongVal5A4 = [];
-
-// ========--counters (correct) (incorrect) (outoftime) ------------
-let correct = 0;
-let wrong = 0;
-let tookTooLong = 0;
-
-// ======================================================
-//======================= FUNCTIONS===============
-// ======================================================
-
-// function to start and restart game
-
-startGame()
-
-//======----timers--(30S) (5S)--------
-
-// ======================================================
-// =======================LOOPS?============
-// ======================================================
+// ----------------------------------------------
+// choose set of question & answers
+//     display the question & nswers 
 
 
-// ======================================================
-// =====================LOGIC (conditionals)=================
-// ======================================================
+// var displayQuestions = function pickPageToDisplay()
+
+// ----------------------------------------------
+// replaceWith()
 
 
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// store function to start the game in variable, or 
-// event listener
+
+$(document).ready(function() {
+
+    $("<button>start</button>").insertAfter("#start");
+})
+
+
+$("<button>start</button>").insertAfter("#start");
+}
+
+
+
+
+
+// $("#bttn").on("click", evaluate);
+
+
+//   --- after 30 seconds it moves to next q & increases unanswered count 
+var thirtySec = setTimeout(unansweredThirtySec, 1000 * 30); 
+
+function unansweredThirtySec() { 
+    if (correct++ || incorrect++) {
+        clearTimeout(unansweredThirtySec);
+    }
+}
+
+
+function unansweredThirtySec() {
+    // start q      or      summary 
+    if ((currentNumOfQs === 0) || (currentNumOfQs === 6)) {
+        clearTimeout(thirtySec);
+
+    }
+}
+
+
+
+
+// =========================================================
+// +++++++++++DOESN'T WORK / NOT FINISHED++++++++++++++++++++
+
+
+// -------specific to the page-----
+//  ----will hold the correct answer, followed by wrong options 
+var pageSpecificArray = [];
+
+// function objectToArray() {
+//     $this.pageSpecificArray.push(this.answer);
+//     this.pageSpecificArray.push(this.wrongOne);
+//     this.pageSpecificArray.push(this.wrongTwo);
+//     this.pageSpecificArray.push(this.wrongThree);
+// }
+
+function objectToArray() {
+    pageSpecificArray.push(this.answer);
+    pageSpecificArray.push(this.wrongOne);
+    pageSpecificArray.push(this.wrongTwo);
+    pageSpecificArray.push(this.wrongThree);}
+
+var qOne;
+var qTwo;
+var qThree;
+var qFour;
+var qFive;
+var qSix;    
+
+// declaring the Object Constructor function, createQuestionAnswerObj 
+// Could have made each Object, hopefully this approach will be useful in future applications
+function createQuestionAnswerObj(question, answer, wrongOne, wrongTwo, wrongThree) {
+    this.question = question;
+    this.answer = answer;
+    this.wrongOne = wrongOne;
+    this.wrongTwo = wrongTwo;
+    this.wrongThree = wrongThree;
+}
+
+// calling the Obj Constructor function
+qOne = new createQuestionAnswerObj("What color is associated with Team Valor?","Red", "Blue", "Yellow", "Green");
+qTwo = new createQuestionAnswerObj("Pokemon Go generated $__ million in revenue its first month","200", "50", "150", "100");
+qThree = new createQuestionAnswerObj("What name guarantees Eevee becoming Flareon?", "Pyro", "Rainer", "Spicy", "Sparky");
+qFour = new createQuestionAnswerObj("Who is the Yellow team's mascot?", "Zapdos", "Spark", "Pikachu", "Instinct");
+qFive = new createQuestionAnswerObj("What berry makes it easier to catch a pokemon", "razz berry", "pinap berry", "lucky berry", "blueberry");
+qSix = new createQuestionAnswerObj("What is the highest level a trainer can reach?", "40", "35", "75", "100");
+
+
+console.log(qOne.question);
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++
+// window.onload = function () {
+
