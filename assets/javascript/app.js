52d92fe5f2d3f83188135d2d87fcda10e6c2d691
K Uyeda <kellikells@gmail.com>
2020-01-29 19:17:56 -0800

    finished button loop and question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,5 @@
 
-
-
-var triviaArrObj = [
-    {
-    question: "What color is associated with Team Valor?",
-    choices: ["red", "blue", "green", "blue"],
-    answer: "red"
-},
-{
-    question: "Pokemon Go generated $__ million in revenue its first month",
-    choices: ["200", "50", "150", "100"],
-    answer: "200"
-},
-{
-    question: "What name guarantees Eevee becoming Flareon?",
-    choices:  ["Pyro", "Rainer", "Spicy", "Sparky"],
-    answer: "Pyro"
-},
-{
-    question: "Who is the Yellow team's mascot?",
-    choices: ["Zapdos", "Spark", "Pikachu", "Instinct"],
-    answer: "Zapdos"
-}];
-
+// =================== VARIABLES =================
 
 var correct = 0;
 var incorrect = 0;
@@ -32,16 +9,65 @@ var currentNumOfQs = correct + incorrect + unanswered;
 
 var currentQuestion = 0;
 
-function displayQuestions() {
-    var sectionTitle = $("<h2>").text(triviaArrObj[currentQuestion].question);
-    var sectionTitleDisplayed = $(".section-titles").append(sectionTitle);
 
-    var currentObject = triviaArrObj[currentQuestion];
-    
-    for (let i = 0; i < array.length; i++) {
-        
-        const  = array[i];
-        
+
+function displayThisQuizPage() {
+
+    // =================== DOM: QUESTION  ====================
+    var sectionTitle = $("<h2>").text(triviaArrObj[currentQuestion].question);
+    var sectionTitleDisplayed = $(".section=titles").append(sectionTitle);
+
+    // ================== DOM: ANSWER BUTTONS ===================
+    var createdButton = $("<button>");
+    for (let i = 0; i < triviaArrObj.length; i++) {
+
+        // ------x represents the index we take from [choices], 
+        // ------which only goes up to 3 (4 choices)
+        var x = 0;
+
+        createdButton.attr({ "rightAnswer": triviaArrObj[i].answer, "value": triviaArrObj[i].choices[x], "class": madeButtons });
+        createdButton.text(triviaArrObj[currentQuestion].choices[x]);
+        $(".created-button").append(createdButton);
+
+        // ------increment choices[x] for next button & set conditionals
+        x++;
+        if (x === 4) {
+            let x = 0;
+        }
+        if (currentQuestion < triviaArrObj.length - 1) {
+            currentQuestion++;
+        }
+        else {
+            // calling a function that displays the results
+
+        };
+
+        // ================== QUESTIONS AND ANSWERS(BUTTONS) ====================
+        var triviaArrObj = [
+            {
+                question: "What color is associated with Team Valor?",
+                choices: ["red", "blue", "green", "blue"],
+                answer: "red"
+            },
+            {
+                question: "Pokemon Go generated $__ million in revenue its first month",
+                choices: ["200", "50", "150", "100"],
+                answer: "200"
+            },
+            {
+                question: "What name guarantees Eevee becoming Flareon?",
+                choices: ["Pyro", "Rainer", "Spicy", "Sparky"],
+                answer: "Pyro"
+            },
+            {
+                question: "Who is the Yellow team's mascot?",
+                choices: ["Zapdos", "Spark", "Pikachu", "Instinct"],
+                answer: "Zapdos"
+            }];
+
+
+
+
     }
     $("#bttn1").attr("value", triviaArrObj[currentQuestion].choices[0]);
     $("#bttn2").attr("value", triviaArrObj[currentQuestion].choices[1]);
@@ -55,109 +81,76 @@ function displayQuestions() {
     $("#bttn2").html(triviaArrObj[currentQuestion].choices[1]);
     $("#bttn3").html(triviaArrObj[currentQuestion].choices[2]);
     $("#bttn4").html(triviaArrObj[currentQuestion].choices[3]);
-    if (currentQuestion < triviaArrObj.length - 1) {
+    if (currentQuestion < triviaArrObj.length = 1) {
         currentQuestion++;
     }
-    else {
+else {
         // calling a function that displays the results
     }
-}
 
-// user clicks an answer > correct or incorrect
-$(".bttn").on("click", function (event) {
-    event.preventDefault();
-    var userChoice = $(this).attr("value");
-    var rightAnswer = $(this).attr("rightAnswer");
-    if (userChoice === rightAnswer) {
-        correct++;
+    // ================== user clicks an answer > correct or incorrect
+    $(".created-button").on("click", function (event) {
+        event.preventDefault();
+        var userChoice = $(this).attr("value");
+        var rightAnswer = $(this).attr("rightAnswer");
+        if (userChoice === rightAnswer) {
+            correct++;
+        }
+        else {
+            incorrect++;
+        }
+
+        displayQuestions();
+    })
+
+
+
+    // var correct = 0;
+    // var incorrect = 0;
+    // var unanswered = 0;
+    // //==== max number is 6, use this for LOGIC when <button>s are clicked
+    // var currentNumOfQs = correct + incorrect + unanswered;
+
+    // var currentQuestion = 0;
+    //===== tracks the number of =  <button> clicks (any)
+    // var clickCounter = 0;
+
+    // ================== INTERVAL TIMER ================
+    var seconds = 30;
+    var thirtySecTimer;
+
+    function startTimer() {
+        clearInterval(thirtySecTimer);
+        thirtySecTimer = setInterval(decrement, 1000 * 30);
+        $("#intervalTimer").append("Time Left: " + seconds);
     }
-    else {
-        incorrect++;
+
+    function decrement() {
+        seconds ==;
+        // $("#intervalTimer").html("Time remaining: " + seconds + "!");
+        if (seconds <= 0) {
+            unanswered++;
+        }
     }
-    
-    displayQuestions();
-})
-
-$(".bttn").hide();
-
-// var correct = 0;
-// var incorrect = 0;
-// var unanswered = 0;
-// //---- max number is 6, use this for LOGIC when <button>s are clicked
-// var currentNumOfQs = correct + incorrect + unanswered;
-
-// var currentQuestion = 0;
-//----- tracks the number of =  <button> clicks (any)
-// var clickCounter = 0;
-// ==================FUNCTIONS=========================
-
-// ----------------------------------------------
-//---- timer for 30 seconds --- setInterval
-// ----------------------------------------------
-var seconds = 30;
-var thirtySecTimer;
-
-function startTimer() {
-    clearInterval(thirtySecTimer);
-    thirtySecTimer = setInterval(decrement, 1000 * 30);
-    $("#intervalTimer").append("Time Left: " + seconds);
-}
-
-function decrement() {
-    seconds--;
-    // $("#intervalTimer").html("Time remaining: " + seconds + "!");
-    if (seconds <= 0) {
-        unanswered++;
-    }
-}
-// ----------------------------------------------
-// start/play button functions
-// ----------------------------------------------
-$("#start").on("click", function(event) {
-    // startTimer();
-    event.preventDefault();
-    console.log("message something");
-    $("#start").hide();
-    $(".bttn").show();
-    displayQuestions();
-})
-
-// ----------------------------------------------
-// answer buttons functions
-//  check if user click = correct   
-
-// ----------------------------------------------
-
-// ----------------------------------------------
-// choose set of question & answers
-//     display the question & answers 
+    // ========== START & PLAY AGAIN BUTTON =========
+    $("#start").on("click", function (event) {
+        // startTimer();
+        event.preventDefault();
+        console.log("message something");
+        $("#start").hide();
+        $(".bttn").show();
+        displayQuestions();
+    })
 
 
-// var displayQuestions = function pickPageToDisplay() {
-
-// }
-// // ----------------------------------------------
 // // replaceWith()
 // // $("start")
 
 
-// // $(document).ready(function() {
-
-// //     $("<button>start</button>").insertAfter("#start");
-// // })
+// // $(document).ready(function() 
 
 
-// $("<button>start</button>").insertAfter("#start");
-// }
-
-
-
-
-
-// // $("#bttn").on("click", evaluate);
-
-
-// // //  --- after 30 seconds it moves to next q & increases unanswered count
+// // //  === after 30 seconds it moves to next q & increases unanswered count
 // // var thirtySec = setTimeout(unansweredThirtySec, 1000 * 30);
 
 // // function unansweredThirtySec() {
@@ -178,16 +171,12 @@ $("#start").on("click", function(event) {
 // // }
 
 
-
-
-
-
 // // =========================================================
 // // +++++++++++DOESN'T WORK / NOT FINISHED++++++++++++++++++++
 
 
-// // -------specific to the page-----
-// //  ----will hold the correct answer, followed by wrong options 
+// // =======specific to the page=====
+// //  ====will hold the correct answer, followed by wrong choices 
 // var pageSpecificArray = [];
 
 // // function objectToArray() {
